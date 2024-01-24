989234864ec685d944b9e31c2615fa3a24abd0cd
nesk-dev <157194595+nesk-dev@users.noreply.github.com>
2024-01-24 23:51:49 +0530

    added roles and users tables

--- nesk-webapi/Migrations/20240124181959_addedUsersAndRoleTables.Designer.cs
+++ nesk-webapi/Migrations/20240124181959_addedUsersAndRoleTables.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using nesk_webapi.AppDbContext;
 
@@ -10,9 +11,11 @@ using nesk_webapi.AppDbContext;
 namespace nesk_webapi.Migrations
 {
     [DbContext(typeof(neskDbContext))]
-    partial class neskDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240124181959_addedUsersAndRoleTables")]
+    partial class addedUsersAndRoleTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ nesk-webapi/Models/Users.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace nesk_webapi.Models
+{
+    public class Users
+    {
+        #region Properties
+        [Key]
+        public Guid PkUsersId { get; set; }
+        public string UserName { get; set; }
+        public string? EmailAddress { get; set; }
+        public string? MobileNumber { get; set; }
+        public string? Password { get; set; }
+        public string? Address { get; set; }
+        public DateTime? CreatedAt { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsDelete { get; set; }
+        #endregion
+
+        #region Associations
+        [ForeignKey("FkRoleId")]
+        public Roles Roles { get; set; }
+        public Guid FkRoleId { get; set; }    
+        #endregion
+    }
+}
