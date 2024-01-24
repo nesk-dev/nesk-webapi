8cee63e1f5f70f1af048bba11afc90df965462c6
nesk-dev <157194595+nesk-dev@users.noreply.github.com>
2024-01-24 22:50:48 +0530

    1. db setup 2. models and db context

---
+++ nesk-webapi/AppDbContext/neskDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using nesk_webapi.Models;
+
+namespace nesk_webapi.AppDbContext
+{
+    public class neskDbContext:DbContext
+    {
+        public neskDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Roles> Roles { get; set; }
+    }
+}
---
+++ nesk-webapi/Migrations/20240124171655_intial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace nesk_webapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class intial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    PkRoleId = table.Column<Guid>(type: "char(36)", nullable: false, collation: "ascii_general_ci"),
+                    RoleName = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.PkRoleId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
